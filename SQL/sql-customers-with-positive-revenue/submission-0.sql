80516315a133b1187c9429eead041aa04942756b
Rahul Ranjan <rahul.ranjanx47@gmail.com>
2026-07-18 01:35:01 +0530

    Delete SQL/sql-customers-with-positive-revenue directory

--- SQL/sql-customers-with-positive-revenue/submission-0.sql
+++
@@ -1,2 +0,0 @@
--- Write your query below
-SELECT DISTINCT customer_id FROM customers WHERE revenue > 0;
--- SQL/sql-customers-with-positive-revenue/submission-1.sql
+++
@@ -1,2 +0,0 @@
--- Write your query below
-SELECT DISTINCT customer_id FROM customers WHERE year = 2020 AND revenue > 0;
